016a0d1d2b000bd907ca3aca52d9ad8dc4500ff4
Michael Synan <hello@michaelsynan.com>
2026-08-23 05:54:30 -0400

    Feat: auth maybe

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,13 @@ export default defineNuxtConfig({
 
   css: ['~/assets/css/main.css'],
 
+  app: {
+    pageTransition: { 
+      name: 'page', 
+      mode: 'out-in' 
+    }
+  },
+
   routeRules: {
     '/': { prerender: true }
   },
